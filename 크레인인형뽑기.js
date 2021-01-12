27aa44682777f4e044a3ddb6462a8ee8c1d0802b
schw240 <schw240@gmail.com>
2021-01-12 21:18:29 +0900

    프로그래머스 크레인인형뽑기 JS

---
+++ 크레인인형뽑기.js
@@ -0,0 +1,24 @@
+function solution(board, moves) {
+
+    var count = 0;
+    var stack = [];
+
+    for(var i=0;i<moves.length;i++){
+        var now = moves[i]-1
+        for(var j=0;j<board.length;j++){
+            if(board[j][now]!=0){
+                if(stack[stack.length-1]===board[j][now]){
+                    stack.pop();
+                    count+=2;
+                }
+                else{
+                    stack.push(board[j][now])
+                }
+                board[j][now] = 0;
+                break;
+            }
+        }
+    }
+    console.log(stack)
+    return count
+}
