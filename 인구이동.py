bff9f7720eb5dab8eaaa78e2c3c3dbdbcf8b5cf1
schw240 <schw240@gmail.com>
2021-04-20 22:49:53 +0900

    dfs/bfs 문제풀이

--- 인구이동.py
+++ 인구이동.py
@@ -12,43 +12,57 @@ import sys
 input = sys.stdin.readline
 
 N, L, R = map(int, input().split())
-A = [list(map(int, input().spli())) for _ in range(N)]
+A = [list(map(int, input().split())) for _ in range(N)]
 
-ans = 0
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
 
 
-def bfs(x, y):
-    move_q = deque()
-    q.append([x, y])
-    c[x][y] = 1
-    people, cnt = 0, 0
+def bfs(row, col):
+    q = deque()
+    q.append([row, col])
+
+    tmp = []
+    tmp.append([row, col])
+
     while q:
         x, y = q.popleft()
-        move_q.append([x, y])
-        people += a[x][y]
-        cnt += 1
+
         for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx < N and 0 <= ny < N and not c[nx][ny]:
+            nx, ny = x + dx[i], y + dy[i]
+
+            if 0 <= nx < N and 0 <= ny < N:
+                if not visited[nx][ny]:
+                    # 인구차이 안에 있다면 국경선 오픈
+                    if L <= abs(A[nx][ny] - A[x][y]) <= R:
+                        visited[nx][ny] = True
+                        q.append([nx, ny])
+                        tmp.append([nx, ny])
+    return tmp
 
 
-def bfs(row, col):
+cnt = 0
+while True:
+    flag = False
+    visited = [[False] * N for _ in range(N)]
+
+    for i in range(N):
+        for j in range(N):
+            if not visited[i][j]:
+                visited[i][j] = True
+                # 인구 배치가 이뤄져야 하는 나라 tmp
+                tmp = bfs(i, j)
+                # print(tmp)
+
+                if len(tmp) > 1:
+                    flag = True
+                    num = sum([A[x][y] for x, y in tmp]) // len(tmp)
+
+                    for x, y in tmp:
+                        A[x][y] = num
+    if not flag:
+        break
+    cnt += 1
 
 
-def move():
-    # 인구이동
-    cnt, pre_cnt = 0, 0
-    while True:
-        is_Move = False
-        total_visited = set()
-        temp = []
-
-        for i in range(N):
-            for j in range(N):
-                if (i, j) not in total_visited:
-
-
-print(move())
+print(cnt)
