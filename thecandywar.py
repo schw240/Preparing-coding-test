a5eef217e75308a115f43716ae7fc9578b55f019
schw240 <schw240@gmail.com>
2021-05-14 20:57:08 +0900

    스터디 문제풀이

--- thecandywar.py
+++ thecandywar.py
@@ -1,42 +1,30 @@
-# 아이들의 캔디를 하나씩 확인
-def check(N, candy):
-    for i in range(N):
-        # 만약 아이의 캔디중 홀수개가 있다면
-        if candy[i] % 2 == 1:
-            # 처음부터 사탕을 가진 경우 보충
-            candy[i] += 1
-    # set은 중복제거해줌 근데 len함수를 이용해 길이가 1이라고하면
-    # set안의 모든 수가 같다는 의미
-    return len(set(candy)) == 1
-
-
 def teacher(N, candy):
-    tmp_list = [0 for i in range(N)]
-    for idx in range(N):
-        if candy[idx] % 2:
-            candy[idx] += 1
-        # 오른쪽 친구의 사탕값은 내가 가진것의 절반
-        candy[idx] = candy[idx] // 2
-        tmp_list[(idx+1) % N] = candy[idx]//2
-
-    for idx in range(N):
-        candy[idx] += tmp_list[idx]
-
+    for i in range(N):
+        if candy[i] % 2 == 1:
+            candy[i] += 1
     return candy
 
 
-def process():
-    # 각 프로세스마다 아이의 수, 현재 아이들이 가지고있는 캔디 배열 받기
-    N, candy = int(input()), list(map(int, input().split()))
+def sharingCandy(N, candy):
     cnt = 0
-    # check함수를 활용하여 현재 아이의 수와 캔디 배열을 체크
-    while not check(N, candy):
+    while len(set(candy)) != 1:
+        children = []
         cnt += 1
-        candy = teacher(N, candy)
-    print(cnt)
+        for i in range(N):
+            tmp = candy[i] // 2
+            candy[i] -= tmp
+            children.append(tmp)
+
+        for j in range(-1, N - 1):
+            candy[j+1] += children[j]
+
+        teacher(N, candy)
+    return cnt
 
 
-# 맨 처음 입력받는 테스트케이스만큼 process 반복한다.
 T = int(input())
 for i in range(T):
-    process()
+    N, candy = int(input()), list(map(int, input().split(' ')))
+    candy = teacher(N, candy)
+    ans = sharingCandy(N, candy)
+    print(ans)
--- 정수삼각형.py
+++ 정수삼각형.py
@@ -1,15 +1,36 @@
-def solution(triangle):
-    #점화식 
-    #value[row][idx] += max(value[row-1][idx-1, value[row-1][idx]])
-    for i in range(1, len(triangle)):
-        for j in range(i+1):
-            #왼쪽값인경우
-            if j == 0:
-                triangle[i][j] += triangle[i-1][j]
-            #오른쪽값인경우
-            elif j == i:
-                triangle[i][j] += triangle[i-1][j-1]
-            else:
-                triangle[i][j] += max(triangle[i-1][j], triangle[i-1][j-1])
-    #마지막층의 최대값 출력
-    return max(triangle[-1])
+# 삼각형 크기 n
+n = int(input())
+triangle = []
+for i in range(n):
+    nums = [0]
+    tmp = list(map(int, input().split(' ')))
+    tmp.append(0)
+    nums.extend(tmp)
+    triangle.append(nums)
+
+# print(triangle)
+
+# 바로위 또는 왼쪽 위 또는 둘중 큰값
+for i in range(1, n):
+    for j in range(i+1):
+        triangle[i][j] += max(triangle[i-1][j], triangle[i-1][j-1])
+
+print(triangle, "triangle")
+print(max(triangle[n-1]))
+
+# # 둘째줄부터 시작
+# for i in range(1, n):
+#     # 각 줄에서 값 하나씩 확인
+#     for j in range(i+1):
+#         # 왼쪽인 경우
+#         if j == 0:
+#             triangle[i][j] += triangle[i-1][j]
+#         # 오른쪽인 경우
+#         elif j == i:
+#             triangle[i][j] += triangle[i-1][j-1]
+#         # 가운대 값들인 경우
+#         else:
+#             triangle[i][j] += max(triangle[i-1][j], triangle[i-1][j-1])
+
+# # print(triangle, "triangle")
+# print(max(triangle[n-1]))
