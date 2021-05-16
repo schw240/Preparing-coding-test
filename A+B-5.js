a5dd791db6ad2b37ffb45c495eac541dcbcda95d
schw240 <schw240@gmail.com>
2021-05-16 21:49:57 +0900

    문제풀이

--- A+B-5.js
+++ A+B-5.js
@@ -1,1 +1,16 @@
-const 
+const readline = require('readline')
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+})
+
+const input = [];
+
+rl.on('line', function(line) {
+    input.push(line)
+}).on('close', function() {
+    while(input[i].split(' ')[0] !== 0 && input[i].split(' ')[1]) {
+        console.log(input[i].split(' ')[0] + input[i].split(' ')[1])
+    }
+    process.exit()
+})``
